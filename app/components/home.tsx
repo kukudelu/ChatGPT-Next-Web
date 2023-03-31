edb5d819ba48218ee190440fcbc08574b8ce99fc
kukudelu <122708320+kukudelu@users.noreply.github.com>
2023-03-31 17:04:54 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -627,9 +627,9 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>ChatGPT AiDown</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            個人AI助手！
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
